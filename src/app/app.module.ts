0dd56db80e6b1f3e4d2a86cff7f41f988fb30070
gauravbnsl <gauravbansal67@hotmail.com>
2020-06-09 21:58:09 +0530

    added form structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
-import { AngularFormsComponent } from './angular-forms/angular-forms.component';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
+import { AngularFormsComponent } from "./angular-forms/angular-forms.component";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule ],
-  declarations: [ AppComponent,AngularFormsComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule
+  ],
+  declarations: [AppComponent, AngularFormsComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
